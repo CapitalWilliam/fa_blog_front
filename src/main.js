9a01b739f53e895ac8bc842c00cd3cff45fd362a
CapitalWilliam <wzf_2023@outlook.com>
2023-01-26 23:46:41 +0800

    init - add plugins

--- src/main.js
+++ src/main.js
@@ -1,16 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import naive from 'naive-ui'
+import {createPinia} from "pinia";
 
-
-/**
- * axios
- * pinia
- * sass
- * vue-router
- * naive-ui
- * wangeditor
- *
- *
- */
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(naive)
+app.use(createPinia())
+app.mount('#app')
